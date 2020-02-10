a75c9aead4f63dd8208d7f1f0815e478e5cd1aee
Andrew LeFevre <jalefevre@liberty.edu>
2020-02-10 01:14:36 -0500

    changed numeric types' truthiness to be NOT 0, rather than greater than 0

--- object/char.go
+++ object/char.go
@@ -5,5 +5,5 @@ import (
 )
 
 func (c Char) Type() ObjectType { return CHAR_OBJ }
-func (c Char) Truthy() bool     { return rune(c) > 0 }
+func (c Char) Truthy() bool     { return rune(c) != 0 }
 func (c Char) Repr() string     { return strconv.QuoteRuneToGraphic(rune(c)) }
--- object/int.go
+++ object/int.go
@@ -5,5 +5,5 @@ import (
 )
 
 func (i Int) Type() ObjectType { return INTEGER_OBJ }
-func (i Int) Truthy() bool     { return int64(i) > 0 }
+func (i Int) Truthy() bool     { return int64(i) != 0 }
 func (i Int) Repr() string     { return strconv.FormatInt(int64(i), 10) }
